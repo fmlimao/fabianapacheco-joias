eabc6cfba138d0f0cd4197657ba693af6188105e
Leandro Macedo <fmlimao@gmail.com>
2022-03-30 12:10:21 -0300

    Cadastrando produtos

---
+++ products.js
@@ -0,0 +1,92 @@
+var products = [
+  {
+    id: 'P0001',
+    title: 'COLAR CHOKER CORRENTE CORAÇÃO VAZADO FOLHEADO A OURO 18K',
+    price: '14,99',
+    images: [ '/img/products/P0001-01.webp' ]
+  },
+  {
+    id: 'P0002',
+    title: 'CONJUNTO COLAR E BRINCO PRATEADO',
+    price: '21,00',
+    images: []
+  },
+  {
+    id: 'P0003',
+    title: 'CONJUNTO COLAR E BRINCO FOLHEADO A OURO 18K',
+    price: '21,00',
+    images: [ '/img/products/P0003-01.webp' ]
+  },
+  {
+    id: 'P0004',
+    title: 'COLAR DELICADO COM PINGENTE DE BOLINHA LISA PEQUENA',
+    price: '14,99',
+    images: [ '/img/products/P0004-01.webp' ]
+  },
+  {
+    id: 'P0005',
+    title: 'COLAR CHOKER CORRENTE DE BOLINHA COM PINGENTE',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0006',
+    title: 'COLAR PINGENTE CONCHA E BERLOQUES FOLHEADO A OURO 18K',
+    price: '14,99',
+    images: [ '/img/products/P0006-01.webp' ]
+  },
+  {
+    id: 'P0007',
+    title: 'COLAR PINGENTE GOLFINHOS PRATEADO',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0008',
+    title: 'COLAR PINGENTE BORBOLETA E BERLOQUES FOLHEADO A OURO 18K',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0009',
+    title: 'COLAR CHOKER COM PINGENTE CONCHA FOLHEADO A OURO 18K',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0010',
+    title: 'COLAR CORRENTE DE BOLINHA',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0011',
+    title: 'COLAR CURTO DE FOLHAS LISA FOLHEADO A OURO 18K',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0012',
+    title: 'BRINCO ARGOLINHA COM PEDRA FORMATO FLOR FOLHEADO A OURO 18K',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0013',
+    title: 'BRINCO ARGOLINHA FECHADA GOTA CRISTAL FOLHEADO A OURO 18 K',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0014',
+    title: 'BRINCO DE CORAÇÃO PRATEADO',
+    price: '14,99',
+    images: []
+  },
+  {
+    id: 'P0015',
+    title: 'BRINCO DE ARGOLAS PRATEADO',
+    price: '14,99',
+    images: []
+  },
+]
--- script.js
+++ script.js
@@ -1,64 +1,7 @@
 var App = new Vue({
   el: '#AppVue',
   data: {
-    list: [
-      {
-        id: 1,
-        title: 'Joia 1',
-        description: 'Descrição da Joia 1',
-        price: '1.000,00',
-        images: [ '/img/j1-400.jpg' ]
-      },
-      {
-        id: 2,
-        title: 'Joia 2',
-        description: 'Descrição da Joia 2',
-        price: '1.000,00',
-        images: [ '/img/j3-400.jpg' ]
-      },
-      {
-        id: 3,
-        title: 'Joia 3',
-        description: 'Descrição da Joia 3',
-        price: '1.000,00',
-        images: [ '/img/j3-400.jpg' ]
-      },
-      {
-        id: 4,
-        title: 'Joia 4',
-        description: 'Descrição da Joia 4',
-        price: '1.000,00',
-        images: [ '/img/j4-400.jpg' ]
-      },
-      {
-        id: 5,
-        title: 'Joia 5',
-        description: 'Descrição da Joia 5',
-        price: '1.000,00',
-        images: [ '/img/j1-400.jpg' ]
-      },
-      {
-        id: 6,
-        title: 'Joia 6',
-        description: 'Descrição da Joia 6',
-        price: '1.000,00',
-        images: [ '/img/j2-400.jpg' ]
-      },
-      {
-        id: 7,
-        title: 'Joia 7',
-        description: 'Descrição da Joia 7',
-        price: '1.000,00',
-        images: [ '/img/j3-400.jpg' ]
-      },
-      {
-        id: 8,
-        title: 'Joia 8',
-        description: 'Descrição da Joia 8',
-        price: '1.000,00',
-        images: [ '/img/j4-400.jpg' ]
-      },
-    ]
+    list: products
   },
   methods: {}
 })
