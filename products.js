1f56f75691284dcae4fe7646afad8c4cf7a6551b
Leandro Macedo <fmlimao@gmail.com>
2022-03-30 14:03:20 -0300

    Adicionando filtro

--- products.js
+++ products.js
@@ -3,90 +3,105 @@ var products = [
     id: 'P0001',
     title: 'COLAR CHOKER CORRENTE CORAÇÃO VAZADO FOLHEADO A OURO 18K',
     price: '14,99',
-    images: [ '/img/products/P0001-01.webp' ]
+    images: ['/img/products/P0001-01.webp'],
+    categories: ['colar', 'corrente'],
   },
   {
     id: 'P0002',
     title: 'CONJUNTO COLAR E BRINCO PRATEADO',
     price: '21,00',
-    images: []
+    images: [],
+    categories: ['colar', 'brinco'],
   },
   {
     id: 'P0003',
     title: 'CONJUNTO COLAR E BRINCO FOLHEADO A OURO 18K',
     price: '21,00',
-    images: [ '/img/products/P0003-01.webp' ]
+    images: ['/img/products/P0003-01.webp'],
+    categories: ['colar', 'brinco'],
   },
   {
     id: 'P0004',
     title: 'COLAR DELICADO COM PINGENTE DE BOLINHA LISA PEQUENA',
     price: '14,99',
-    images: [ '/img/products/P0004-01.webp' ]
+    images: ['/img/products/P0004-01.webp'],
+    categories: ['colar', 'pingente'],
   },
   {
     id: 'P0005',
     title: 'COLAR CHOKER CORRENTE DE BOLINHA COM PINGENTE',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['colar', 'corrente', 'pingente'],
   },
   {
     id: 'P0006',
     title: 'COLAR PINGENTE CONCHA E BERLOQUES FOLHEADO A OURO 18K',
     price: '14,99',
-    images: [ '/img/products/P0006-01.webp' ]
+    images: ['/img/products/P0006-01.webp'],
+    categories: ['colar', 'pingente'],
   },
   {
     id: 'P0007',
     title: 'COLAR PINGENTE GOLFINHOS PRATEADO',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['colar', 'pingente'],
   },
   {
     id: 'P0008',
     title: 'COLAR PINGENTE BORBOLETA E BERLOQUES FOLHEADO A OURO 18K',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['colar', 'pingente'],
   },
   {
     id: 'P0009',
     title: 'COLAR CHOKER COM PINGENTE CONCHA FOLHEADO A OURO 18K',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['colar', 'pingente'],
   },
   {
     id: 'P0010',
     title: 'COLAR CORRENTE DE BOLINHA',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['colar', 'corrente'],
   },
   {
     id: 'P0011',
     title: 'COLAR CURTO DE FOLHAS LISA FOLHEADO A OURO 18K',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['colar'],
   },
   {
     id: 'P0012',
     title: 'BRINCO ARGOLINHA COM PEDRA FORMATO FLOR FOLHEADO A OURO 18K',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['brinco'],
   },
   {
     id: 'P0013',
     title: 'BRINCO ARGOLINHA FECHADA GOTA CRISTAL FOLHEADO A OURO 18 K',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['brinco'],
   },
   {
     id: 'P0014',
     title: 'BRINCO DE CORAÇÃO PRATEADO',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['brinco'],
   },
   {
     id: 'P0015',
     title: 'BRINCO DE ARGOLAS PRATEADO',
     price: '14,99',
-    images: []
+    images: [],
+    categories: ['brinco'],
   },
 ]
--- script.js
+++ script.js
@@ -1,7 +1,63 @@
 var App = new Vue({
   el: '#AppVue',
   data: {
-    list: products
+    list: products,
+    products: products,
+    categories: {}
   },
-  methods: {}
+  methods: {
+    getCategories: function () {
+      const categories = {}
+
+      for (const product of App.products) {
+        for (const category of product.categories) {
+          if (categories[category] === undefined) {
+            categories[category] = {
+              name: category,
+              amount: 0,
+              selected: false
+            }
+          }
+
+          categories[category].amount++;
+        }
+      }
+
+      App.categories = categories
+    },
+
+    toFilter: function () {
+
+      const selectedCategories = []
+
+      for (const category in App.categories) {
+        if (App.categories[category].selected) {
+          selectedCategories.push(category)
+        }
+      }
+
+      if (selectedCategories.length) {
+        App.list = []
+
+        for (const product of App.products) {
+          for (const category of product.categories) {
+            if (selectedCategories.indexOf(category) !== -1) {
+              App.list.push(product)
+              break
+            }
+          }
+        }
+      } else {
+        App.list = App.products
+      }
+    }
+  }
 })
+
+App.getCategories();
+
+setTimeout(() => {
+  // App.categories['colar'].selected = true
+  App.categories['brinco'].selected = true
+  App.toFilter()
+}, 500)
